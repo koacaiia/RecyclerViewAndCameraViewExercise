d3b2ad5f77274753b5ce6b28e388bbe40fbb47ff
AdroidYsk <koacaiia@gamil.com>
2021-04-08 11:04:49 +0900

    Initial commit

---
+++ app/src/main/java/fine/koacaiia/recyclerviewandcameraviewexercise/MainActivity.java
@@ -0,0 +1,16 @@
+package fine.koacaiia.recyclerviewandcameraviewexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fine.koacaiia.recyclerviewandcameraviewexercise.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
