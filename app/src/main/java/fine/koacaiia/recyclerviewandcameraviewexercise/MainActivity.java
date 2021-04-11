8a30bcc5f315802f540ac3dd4bd128ac1c8dc492
AdroidYsk <koacaiia@gamil.com>
2021-04-11 14:02:12 +0900

    Camera

--- app/src/main/java/fine/koacaiia/recyclerviewandcameraviewexercise/MainActivity.java
+++ app/src/main/java/fine/koacaiia/recyclerviewandcameraviewexercise/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,20 +20,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import fine.koacaiia.recyclerviewandcameraviewexercise.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
     RecyclerView recyclerview;
     ArrayList<MnfCargoList> list;
     MnfCargoListAdapter adapter;
     FirebaseDatabase database;
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    String[] permission_list={Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions(permission_list,0);
 
         list=new ArrayList<>();
         recyclerview=findViewById(R.id.recyclerView);
@@ -39,6 +51,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerview.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
+        surfaceView=findViewById(R.id.surfaceView);
+        surfaceHolder=surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        preViewProgress();
+
+    }
+
+    private void preViewProgress() {
+        camera=Camera.open();
+        camera.setDisplayOrientation(90);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();;
     }
 
     private void getData() {
@@ -62,5 +91,31 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         databaseReference.addListenerForSingleValueEvent(listener);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int result:grantResults){
+            if(result== PackageManager.PERMISSION_DENIED){
+                Toast.makeText(this,"Permission Denied",Toast.LENGTH_SHORT).show();
+            }
+        }
+        preViewProgress();
     }
 }
